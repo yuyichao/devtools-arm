dddca407e767a4f7cc0f579535ef5d4361dfd8a2
Yichao Yu <yyc1992@gmail.com>
2022-03-19 13:59:51 -0400

    Use the correct version of exec function...

--- archbuild.py
+++ archbuild.py
@@ -7,4 +7,4 @@ if 'MAKEFLAGS' in os.environ:
     os.environ['MAKEFLAGS_saved'] = os.environ['MAKEFLAGS']
 
 # Use python so that we can set $0.
-os.execv('archbuild', sys.argv)
+os.execvp('archbuild', sys.argv)
