5a0992159ce4066bd9c296a3e6a857e686a4544e
Yichao Yu <yyc1992@gmail.com>
2022-03-19 13:50:15 -0400

    [archbuild] Create a python wrapper to work around the MAKEFLAGS override issue from devtools-alarm

---
+++ archbuild.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+if 'MAKEFLAGS' in os.environ:
+    os.environ['MAKEFLAGS_saved'] = os.environ['MAKEFLAGS']
+
+# Use python so that we can set $0.
+os.execv('archbuild', sys.argv)
